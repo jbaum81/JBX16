b3375330a26e9cb3614064ed2defeab46adc2fcd
jbaum81 <jbaum81@gmail.com>
2022-08-22 23:44:30 -0700

    CPLD Code Updates

--- CPLD/JB6502ATF1508PLCC/JB6502ATF1508PLCC.v
+++ CPLD/JB6502ATF1508PLCC/JB6502ATF1508PLCC.v
@@ -11,7 +11,7 @@ module JB6502ATF1508PLCC(
 	output [7:0] adrBanks,
 	output oeLow,
 	output clkWr,
-	output datDD,
+	inout datDD,
 	output raEn,
 	output ioEn,
 	output v0En,
@@ -22,51 +22,11 @@ module JB6502ATF1508PLCC(
 	output hr2En,
 	output hr3En,	
 	output roEn,
-	output ymfEn
+	output ymfEn,
+	output activity
 	);
 	
-	
-	//System Control
-	assign oeLow = (oe) ? ~rw : 1'bz;
-	assign clkWr = (oe) ? ~(sysClk && ~rw) : 1'bz;
-	assign datDD = (oe) ? ~(~_ioEn && _v0En && v1En) : 1'bz;
 
-	
-	//Bank Address Line Controls
-	wire [7:0] _adrBanks = (~_hrEn) ? _raBank : _roBank;
-	assign adrBanks = (oe) ? _adrBanks : 8'bzzzzzzzz;
-	
-	//Rom Enables
-	wire _roEn = (adrBusHi[7:6] == 2'b11) ? 1'b0 : 1'b1;
-	assign roEn = (oe) ? ~(adrBusHi[7:6] == 2'b11) : 1'bz;
-	
-
-	//Banked Ram Enables
-	wire _hrEn = (adrBusHi[7:5] == 3'b101) ? 1'b0 : 1'b1;
-	assign hr0En = (oe) ? ~(_raBank[7:6] == 2'b00 && ~_hrEn) : 1'bz;
-	assign hr1En = (oe) ? ~(_raBank[7:6] == 2'b01 && ~_hrEn) : 1'bz;
-	assign hr2En = (oe) ? ~(_raBank[7:6] == 2'b10 && ~_hrEn) : 1'bz;
-	assign hr3En = (oe) ? ~(_raBank[7:6] == 2'b11 && ~_hrEn) : 1'bz;
-
-
-	//IO enables
-	wire _ioEn = (adrBusHi == 8'b10011111) ? 1'b0 : 1'b1;
-	assign ioEn = (oe) ? _ioEn : 1'bz;
-	wire _v0En = (~_ioEn && adrBusLo[7:4] == 4'b0000 ) ? 1'b0 : 1'b1;
-	assign v0En = (oe) ? _v0En : 1'bz;
-	wire _v1En = (~_ioEn && adrBusLo[7:4] == 4'b0001 ) ? 1'b0 : 1'b1;
-	assign v1En = (oe) ? _v1En : 1'bz;
-	wire _srlEn = (~_ioEn && adrBusLo[7:4] == 4'b0110 ) ? 1'b0 : 1'b1;
-	assign srlEn = (oe) ? _srlEn : 1'bz;
-	
-	wire isYMF = (~_ioEn && adrBusLo[7:1] == 7'b0100000 ) ? 1'b1 : 1'b0;
-	wire _ymfEn = (isYMF && sysClk) ? 1'b0 : 1'b1;
-	assign ymfEn = (oe) ? _ymfEn : 1'bz;
-
-	//Ram Enables
-	wire _raEn = (_roEn && _hrEn && _ioEn) ? 1'b0 : 1'b1;
-	assign raEn = (oe) ? _raEn : 1'bz;
-	
 	//Clocking and Stretching
 	reg clk8sys = 0; 
 	assign sysClk = clk8sys;
@@ -92,10 +52,64 @@ module JB6502ATF1508PLCC(
 			end
 		end
 	end
+	
+	wire _roEn;
+	assign _roEn = (adrBusHi[7:6] == 2'b11) ? 1'b0 : 1'b1;
+	assign roEn = (oe) ? _roEn : 1'bz;
+	
+	wire _hrEn;
+	assign _hrEn = (adrBusHi[7:5] == 3'b101) ? 1'b0 : 1'b1;
+	wire _hr0En;
+	assign _hr0En = (_raBank[7:6] == 2'b00 && ~_hrEn) ? 1'b0 : 1'b1;
+	assign hr0En = (oe) ? _hr0En : 1'bz;
+	wire _hr1En;
+	assign _hr1En = (_raBank[7:6] == 2'b01 && ~_hrEn) ? 1'b0 : 1'b1;
+	assign hr1En = (oe) ? _hr1En : 1'bz;
+	wire _hr2En;
+	assign _hr2En = (_raBank[7:6] == 2'b10 && ~_hrEn) ? 1'b0 : 1'b1;
+	assign hr2En = (oe) ? _hr2En : 1'bz;
+	wire _hr3En;
+	assign _hr3En = (_raBank[7:6] == 2'b11 && ~_hrEn) ? 1'b0 : 1'b1;
+	assign hr3En = (oe) ? _hr3En : 1'bz;
+	
+	wire _ioEn;
+	assign _ioEn = (adrBusHi == 8'b10011111) ? 1'b0 : 1'b1;
+	assign ioEn = (oe) ? _ioEn : 1'bz;
+	wire _v0En;
+	assign _v0En = (~_ioEn && adrBusLo[7:4] == 4'b0000 ) ? 1'b0 : 1'b1;
+	assign v0En = (oe) ? _v0En : 1'bz;
+	wire _v1En;
+	assign _v1En = (~_ioEn && adrBusLo[7:4] == 4'b0001 ) ? 1'b0 : 1'b1;
+	assign v1En = (oe) ? _v1En : 1'bz;
+	wire _srlEn;
+	assign _srlEn = (~_ioEn && adrBusLo[7:4] == 4'b0110 ) ? 1'b0 : 1'b1;
+	assign srlEn = (oe) ? _srlEn : 1'bz;
+	//wire _vidEn = (~_ioEn && adrBusLo[7:5] == 3'b001 ) ? 1'b0 : 1'b1;
+	assign datDD = (~ioEn && rw && sysClk && v1En && v0En) ? 1'b0 : 1'bz;
 
-	//Banked Ram Setup
+	wire isYMF = (~_ioEn && adrBusLo[7:1] == 7'b0100000 ) ? 1'b1 : 1'b0;
+	wire _ymfEn = (isYMF && sysClk) ? 1'b0 : 1'b1;
+	assign ymfEn = (oe) ? _ymfEn : 1'bz;
+	
+	wire _raEn;
+	assign _raEn = (_roEn && _hrEn && _ioEn) ? 1'b0 : 1'b1;
+	assign raEn = (oe) ? _raEn : 1'bz;
+	
+	
+	wire _rLow;
+	assign _rLow = ~rw;
+	assign oeLow = (oe) ? _rLow : 1'bz;
+	
+	wire _clkWr;
+	assign _clkWr = (~rw && sysClk) ? 1'b0 : 1'b1;
+	assign clkWr = (oe) ? _clkWr : 1'bz;
+	
 	reg [7:0] _raBank = 0;
 	reg [7:0] _roBank = 0;
+	wire [7:0] _rBanks;
+	assign _rBanks = (~_hrEn) ? _raBank : _roBank;
+	assign adrBanks = (oe) ? _rBanks : 1'bz;
+	
 	always @(negedge sysClk) begin
 		if (~rst)
 			_raBank <= 0;
@@ -109,8 +123,8 @@ module JB6502ATF1508PLCC(
 		else if (adrBusHi == 0 && adrBusLo == 1 && ~rw)
 			_roBank <= datBus;
 	end
-	
-	
+
 
 	
+
 endmodule
