e2be8fdd3ad785e0f6d4c8d9d28b8bc6b9e1d47c
jbaum81 <jbaum81@gmail.com>
2023-02-07 19:00:42 -0800

    Updates

--- CPLD/VeraCPLD/VeraCPLD.v
+++ CPLD/VeraCPLD/VeraCPLD.v
@@ -1,19 +1,20 @@
 module VeraCPLD(
 	input wire		clk,
    input wire		rw, 
-	input wire [10:0] adr,
+	input wire [11:0] adr,
 	output wire wrB,
 	output wire rdB,
-	output wire datDD,
+	//output wire datDD,
 	output rLED,
 	output wLED
 	);
 	
 	
-	wire en = (adr == 11'b10011111001) ? 0 : 1;
+	wire en = (adr == 12'h9f2 || adr == 12'h9f3) ? 0 : 1;
 	assign rdB = (!en && rw && clk) ? 0 : 1;
 	assign wrB = (!en && !rw && clk) ? 0 : 1;
-	assign datDD = (!en && rw) ? 0 : 1'bz;	
+	//assign datDD = (!en && rw) ? 0 : 1'bz;	
+	assign datDD = 1'bz;	
 	
 	assign rLED = rdB;
 	assign wLED = wrB;
